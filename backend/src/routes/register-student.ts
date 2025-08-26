a6ef8d2ccf7a6609363e3a61585faa4d193f70f6
RianRG <rianrgomes@gmail.com>
2025-08-26 20:04:38 -0300

    feat: update frontend and starting to make solo

--- backend/src/routes/register-student.ts
+++
@@ -1,7 +0,0 @@
-import { FastifyTypedInstance } from "../types.ts"
-
-export async function RegisterStudentRoute(app: FastifyTypedInstance){
-  app.post('/student', async (req, res) =>{
-    return res.status(200).send({ msg: 'hello' })
-  })
-}
--- backend/src/server.ts
+++
@@ -1,8 +0,0 @@
-import { app } from "./app.ts";
-
-if(!process.env.PORT) throw new Error('Variáveis ambientes não disponíveis!');
-
-app.listen({
-  port: +process.env.PORT
-})
-.then(() => console.log('runnin at ' + process.env.PORT));
